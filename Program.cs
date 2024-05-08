80f61f1d981a6d1ee02e9f419568aa2bf559d667
Emil <emil.levin125@gmail.com>
2024-05-08 18:31:27 +0400

    Improved the average char width and heigh estimations

--- Program.cs
+++ Program.cs
@@ -6,10 +6,10 @@ namespace ConsoleGame
     {
         static void Main(string[] args)
         {
-            int MAX_CONSOLE_WIDTH_CHARS = 120;
-            int MAX_CONSOLE_HEIGHT_CHARS = 40;
-            float AVERAGE_CHAR_WIDTH = 9.45f;
-            float AVERAGE_CHAR_HEIGHT = 21.25f;
+            const int MAX_CONSOLE_WIDTH_CHARS = 120;
+            const int MAX_CONSOLE_HEIGHT_CHARS = 40;
+            const float AVERAGE_CHAR_WIDTH = 9.45f;
+            const float AVERAGE_CHAR_HEIGHT = 21f;
 
             ConsoleManager consoleManager = new ConsoleManager(MAX_CONSOLE_WIDTH_CHARS, MAX_CONSOLE_HEIGHT_CHARS, AVERAGE_CHAR_WIDTH, AVERAGE_CHAR_HEIGHT);
             consoleManager.ConfigureDisplay();
