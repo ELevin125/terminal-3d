7b7bf8221fc4c7b6e928b84da858eed0d2d7379f
Emil <emil.levin125@gmail.com>
2024-05-08 22:23:29 +0400

    Added basic Vec2 structure (no methods) and an algorithm to draw a line between two points on the console

--- Renderer.cs
+++ Renderer.cs
@@ -1,4 +1,5 @@
-﻿using Terminal_3D.SceneManagement;
+﻿using Terminal_3D.Geometry;
+using Terminal_3D.SceneManagement;
 
 namespace Terminal_3D.Rendering
 {
@@ -17,8 +18,39 @@ namespace Terminal_3D.Rendering
 
             ConsoleManager.DrawBorder('#');
 
+            DrawLine2D(new Vector2(10, 10), new Vector2(20, 20));
+            DrawLine2D(new Vector2(20, 20), new Vector2(30, 10));
+
             // Hold on last frame
             Console.ReadKey();
+        }
+
+        private void DrawLine2D(Vector2 start, Vector2 end, char c = '#')
+        {
+            int dx = (int)Math.Abs(end.X - start.X);
+            int dy = (int)Math.Abs(end.Y - start.Y);
+            int sx = start.X < end.X ? 1 : -1;
+            int sy = start.Y < end.Y ? 1 : -1;
+            int err = dx - dy;
+
+            Vector2 place = start;
+            
+
+            while (place.X != end.X || place.Y != end.Y)
+            {
+                ConsoleManager.DrawCharacter((int)place.X, (int)place.Y, c);
+                int err2 = 2 * err;
+                if (err2 > -dy)
+                {
+                    err -= dy;
+                    place.X += sx;
+                }
+                if (err2 < dx)
+                {
+                    err += dx;
+                    place.Y += sy;
+                }
+            }
         }
     }
 }
---
+++ Vector2.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Terminal_3D.Geometry
+{
+    public class Vector2
+    {
+        public float X;
+        public float Y;
+
+        public Vector2(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static Vector2 Zero
+        {
+            get { return new Vector2(0, 0); }
+        }
+    }
+}
