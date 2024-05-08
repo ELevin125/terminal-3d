3317281aa299682818c8ca2b37a4cbbfe77ce649
Emil <emil.levin125@gmail.com>
2024-05-08 20:32:39 +0400

    Added basic Entity class for object that can be placed and moved in scenes

---
+++ Entity.cs
@@ -0,0 +1,26 @@
+﻿using Terminal_3D.Geometry;
+
+namespace Terminal_3D.SceneManagement
+{
+    public class Entity
+    {
+        public Vector3 Position { get; set; }
+        public Vector3 Rotation { get; set; }
+
+        public Entity(Vector3 position, Vector3 rotation)
+        {
+            Position = position;
+            Rotation = rotation;
+        }
+
+        public void Move(Vector3 delta)
+        {
+            Position += delta;
+        }
+
+        public void Rotate(Vector3 eulerRotation)
+        {
+            Rotation += eulerRotation;
+        }
+    }
+}
