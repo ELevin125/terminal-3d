ab2a894d21aaf1215265241f347fd708e12b6b81
Emil <emil.levin125@gmail.com>
2024-05-09 11:58:52 +0400

    Added basic mesh class

---
+++ Mesh.cs
@@ -0,0 +1,45 @@
+﻿namespace Terminal_3D.Geometry
+{
+    public struct Vertex
+    {
+        public Vector3 Position;
+
+        public Vertex(Vector3 position)
+        {
+            Position = position;
+        }
+    }
+
+    public struct Edge
+    {
+        public int V1;
+        public int V2;
+
+        public Edge(int v1, int v2)
+        {
+            V1 = v1;
+            V2 = v2;
+        }
+    }
+    public class Mesh
+    {
+        public List<Vertex> Vertices;
+        public List<Edge> Edges;
+
+        public Mesh()
+        {
+            Vertices = new List<Vertex>();
+            Edges = new List<Edge>();
+        }
+
+        public void AddVertex(Vertex vertex)
+        {
+            Vertices.Add(vertex);
+        }
+
+        public void AddEdge(int vertexIndex1, int vertexIndex2)
+        {
+            Edges.Add(new Edge(vertexIndex1, vertexIndex2));
+        }
+    }
+}
